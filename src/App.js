718c2949e7b8842d1416b4c2277163f2ab4ff74f
maremaric <markoreus888@gmail.com>
2022-06-15 21:29:29 +0200

    Fetch tasks from server

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 
   const [showAddTask, setShowAddTask] = useState(false);
 
   const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    }
+    getTasks();
+  }, []);
+
+// Fetch tasks
+const fetchTasks = async () => {
+  const res = await fetch('http://localhost:5000/tasks');
+  const data = await res.json();
+
+  return data;
+}
 
 // Add Task
   const addTask = (task) => {
